7fb95117f887e99e903a9155cc85ae3bd2df5e82
Milotskiy Alexey <spalaxinco@gmail.com>
2013-09-03 13:44:12 +0300

    Remove default loader in Container template.

--- Container.js
+++ Container.js
@@ -4,13 +4,14 @@ define([
     "dojo/_base/lang",
     "dojo/_base/fx",
     "dojo/dom-style",
+    "dojo/query",
     "dijit/_Widget",
     "dijit/_Container",
     "./Slide",
     "./SlideLink",
     "dijit/_TemplatedMixin",
     "dojo/text!./templates/Container.html"
-], function(declare, array, lang, fx, domStyle, _Widget, 
+], function(declare, array, lang, fx, domStyle, query, _Widget,
             _Container, Slide, SlideLink, _TemplatedMixin,
             template) {
     return declare('slideshow.Container', [ _Widget, _Container, _TemplatedMixin ], {
@@ -34,8 +35,12 @@ define([
         buildRendering: function () {
             try {
                 this.inherited(arguments);
-                !this.loading && this.domNode.removeChild(this.loadingNode);
-                this.loading && domStyle.set(this.loadingNode, 'display', '');
+                this.loadingNode = query('.loader', this.domNode)[0];
+
+                if (this.loadingNode) {
+                    domStyle.set(this.loadingNode, 'display', '');
+                }
+
                 domStyle.set(this.containerNode, {'display': 'none',
                                                   'height': this.height+'px',
                                                   'width': this.width+'px',
@@ -90,7 +95,7 @@ define([
         
         _startSlideshow: function () {
             try {
-                this.loading && domStyle.set(this.loadingNode, 'display', 'none');
+                this.loadingNode && domStyle.set(this.loadingNode, 'display', 'none');
                 domStyle.set(this.containerNode, 'display', '');
                 this.nextSlide();
             } catch (e) {
